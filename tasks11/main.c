6c2571b6d673eabe2ffc9daa5916b8fdfd8d9998
Katbut <git@github.com:Katbut/C_MSU_2semester.git>
2024-04-22 20:57:05 +0300

    Initial Commit

---
+++ tasks11/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  tasks11
+//
+//  Created by Екатерина Буторина on 22.04.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
